53ce3b1d88451df91142c5399ffc47517b302d7f
ferconde87 <ferconde87@gmail.com>
2017-07-22 16:58:50 -0300

    Add files via upload
    4.12 
    * fix bug recursive method
    * linear-time algorithm working

--- 4TreesAndGraphs/12PathsWithSum.cpp
+++ 4TreesAndGraphs/12PathsWithSum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 
 //#include "BinaryTree.h"
 
@@ -17,10 +18,17 @@ class Node{
 		}
 };
 
+void printMap(unordered_map<int, int> map){
+	cout << "---------------------" << endl;
+	for(auto it = map.begin(); it != map.end(); it++)
+		cout << it->first << " " << it->second << endl;	
+	cout << "---------------------" << endl;
+}
+
 //int result = 0;
 
 // 1 Recursive method. O(n log n) in a balanced tree
-// 					   O (node ^ 2) inode a nodeonode-balanodeced tree   
+// 					   O (node ^ 2) in a non-balanodeced tree   
 int pathsWithSum(Node<int> * node, int target, int sum){
 	if(node == NULL) return 0;
 	sum += node->data;
@@ -30,33 +38,29 @@ int pathsWithSum(Node<int> * node, int target, int sum){
 	return result;
 }
 
+
 int pathsWithSum(Node<int> * root, int target){
-	// unordered_map<int, int> map;
+	if(root == NULL) return 0;
 	int rootSum = pathsWithSum(root, target, 0);
-	int leftSum = pathsWithSum(root->left, target, 0);
-	int rightSum = pathsWithSum(root->right, target, 0);
+	int leftSum = pathsWithSum(root->left, target);
+	int rightSum = pathsWithSum(root->right, target);
 	return rootSum + leftSum + rightSum;
 }
 
 
 // 2 Using memoization O(n)
-int pathsWithSum2(Node<int> * n, int target, int sum, unordered_map<int, int> & map){
+int pathsWithSum2(Node<int> * n, int target, int sum, unordered_map<int, int> map){
 	if(n == NULL) return 0;
 	int result = 0;
-	
-	cout << n->data << endl;
-	
 	sum += n->data;
 	int needed = sum - target;
 	auto it = map.find(needed); 
 	if(it != map.end()){
 		result += it->second;
 	}
-	//sum += n->data;
 	auto itSum = map.find(sum);
 	if(itSum!=map.end()){
 		itSum->second++;
-		//itSum->second = itSum->second + 1;
 	}else{
 		map[sum] = 1;
 	}
@@ -65,72 +69,87 @@ int pathsWithSum2(Node<int> * n, int target, int sum, unordered_map<int, int> & 
 }
 
 
-
 int pathsWithSum2(Node<int> * root, int target){
 	unordered_map<int, int> map;
+	if(root == NULL) return 0;
+	map[0] = 1;	
+	return pathsWithSum2(root, target, 0, map);
+}
 
-		
-	int res = pathsWithSum2(root, target, 0, map);
-	
-		
-	for(auto it = map.begin(); it != map.end(); it++)
-		cout << it->first << " " << it->second << endl;
-		
-	return res;
+
+int pathsWithSumV(vector<int> v, int target){
+	unordered_map<int, int> map;
+	int sum = 0;
+	int result = 0;
+	for(int i = 0; i < v.size(); i++){
+		sum += v[i];
+		int needed = sum - target;
+		auto itSum = map.find(needed);
+		if(itSum!=map.end())
+			result+= itSum->second;
+		auto it = map.find(sum);
+		if(it != map.end()){
+			it->second++;
+		}else{
+			map[sum] = 1;
+		}
+	}
+	return result;
 }
 
 
 int main(){
 
-	//~ Node<int> * root = new Node<int>(2);
-	//~ Node<int> * node2 = new Node<int>(1);
-	//~ Node<int> * node3 = new Node<int>(2);
-	//~ Node<int> * node4 = new Node<int>(-1);
-	//~ Node<int> * node5 = new Node<int>(2);
-	//~ Node<int> * node6 = new Node<int>(0);
-	//~ Node<int> * node7 = new Node<int>(1);
-	//~ Node<int> * node8 = new Node<int>(2);
-	//~ Node<int> * node9 = new Node<int>(-1);
-	//~ Node<int> * node10 = new Node<int>(1);
-	//~ Node<int> * node11 = new Node<int>(1);
-	//~ 
-	//~ root->left = node2;
-	//~ root->right = node3;
-	//~ node2->left = node4;
-	//~ node2->right = node5;
-	//~ node4->left = node7;
-	//~ node4->right = node8;
-	//~ node7->left = node11;
-	//~ node5->right = node9;
-	//~ node3->right = node6;
-	//~ node6->left = node10;
-
-	//~ cout << pathsWithSum(root, 5) << endl;//2
-	//~ cout << pathsWithSum2(root, 5) << endl;//2
+	Node<int> * root = new Node<int>(2);
+	Node<int> * node2 = new Node<int>(1);
+	Node<int> * node3 = new Node<int>(2);
+	Node<int> * node4 = new Node<int>(-1);
+	Node<int> * node5 = new Node<int>(2);
+	Node<int> * node6 = new Node<int>(0);
+	Node<int> * node7 = new Node<int>(1);
+	Node<int> * node8 = new Node<int>(2);
+	Node<int> * node9 = new Node<int>(-1);
+	Node<int> * node10 = new Node<int>(1);
+	Node<int> * node11 = new Node<int>(1);
 	
-	//~ auto n12 = new Node<int>(1);
-	//~ 
-	//~ n11->right = n12;
-	//~ 
-	//~ cout << pathsWithSum(root, 5) << endl;//3
-	//~ cout << pathsWithSum2(root, 5) << endl;//3
-	//~ 
-	//~ auto n13 = new Node<int>(0);
-	//~ 
-	//~ n12->right = n13;
-	//~ 
-	//~ cout << pathsWithSum(root, 5) << endl;//4
-	//~ cout << pathsWithSum2(root, 5) << endl;//4
-	//~ 
-	//~ auto n14 = new Node<int>(3);
-	//~ 
-	//~ n6->right = n14;
-//~ 
-	//~ cout << pathsWithSum(root, 5) << endl;//5
-	//~ cout << pathsWithSum2(root, 5) << endl;//5
-	//~ 
-	//~ auto n15 = new Node<int>(2);
-	//~ 
+	root->left = node2;
+	root->right = node3;
+	node2->left = node4;
+	node2->right = node5;
+	node4->left = node7;
+	node4->right = node8;
+	node7->left = node11;
+	node5->right = node9;
+	node3->right = node6;
+	node6->left = node10;
+
+	cout << "result\tresult 2" << endl;
+	cout << pathsWithSum(root, 5); cout << "\t";//2
+	cout << pathsWithSum2(root, 5) << endl;//2
+	
+	auto node12 = new Node<int>(1);
+	
+	node11->right = node12;
+	
+	cout << pathsWithSum(root, 5); cout << "\t";//3
+	cout << pathsWithSum2(root, 5) << endl;//3
+	
+	auto node13 = new Node<int>(0);
+	
+	node12->right = node13;
+	
+	cout << pathsWithSum(root, 5); cout << "\t";//4
+	cout << pathsWithSum2(root, 5) << endl;//4
+	
+	auto node14 = new Node<int>(3);
+	
+	node6->right = node14;
+
+	cout << pathsWithSum(root, 5); cout << "\t";//5
+	cout << pathsWithSum2(root, 5) << endl;//5
+	
+	//~ auto node15 = new Node<int>(2);
+	
 	//~ cout << pathsWithSum(root, 5) << endl;//5
 	//~ cout << pathsWithSum2(root, 5) << endl;//5
 	
@@ -151,6 +170,12 @@ int main(){
 	n5b->left = n6;	
 	n6->left = n7;	
 	n7->left = n8;	
-	cout << pathsWithSum2(r2, 5) << endl;//5	
+	cout << pathsWithSum(root, 5); cout << "\t";//5	
+	cout << pathsWithSum2(r2, 8) << endl;//5	
+	
+	//~ cout << "Prueba con un vector" << endl;
+	//~ vector<int> v = {10, 5, 1, 2, -1, -1, 7, 1, 2};
+	//~ cout << pathsWithSumV(v, 8) << endl;
+	
 	return 0;
 }
