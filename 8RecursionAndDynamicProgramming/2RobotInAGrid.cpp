59d9821a8d2c6ff4f3a96e0307c3a98990140f7b
ferconde87 <ferconde87@gmail.com>
2017-08-10 19:29:38 -0300

    Add files via upload
    8.2 y 8.3

--- 8RecursionAndDynamicProgramming/2RobotInAGrid.cpp
+++ 8RecursionAndDynamicProgramming/2RobotInAGrid.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <locale>
 
 #define vi vector<int>
 #define vvi vector< vi >
@@ -8,6 +9,15 @@
 #define CYESNO(x) cout << (x ? "YES" : "NO") << endl;
 
 using namespace std;
+
+void print(vvi & vv, int n, int m){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++)
+			cout << vv[i][j] << "\t";
+		cout << endl;
+	}
+	cout << endl;
+}
 
 bool isOffLimit(int x){
 	return x!=0;
@@ -26,9 +36,36 @@ bool RobotInAGrid(vvi & vv, int n, int m){
 	return RobotInAGrid(vv, 0, 0, n-1, m-1); 
 }
 
-//TopDown
+//BottomUp
 //Complejidad temporal O(n * m) , Complejidad espacial O(n*m)
 bool RobotInAGridDP(vvi & vv, vvi & memo, int i, int j, int n, int m){
+	if(i > n || j > m) return 0;
+	if(i == n && j == m){
+		memo[i][j] = 1;
+		return 1;
+	} 
+	if(memo[i][j] != -1) return memo[i][j];
+	if(isOffLimit(vv[i][j])){
+		memo[i][j] = 0;
+	}else{
+		memo[i][j] = RobotInAGridDP(vv, memo, i+1, j, n, m) || RobotInAGridDP(vv, memo, i, j+1, n, m);
+	}
+	
+	//cout << "(" << i << "," << j << ") =" << memo[i][j] << endl;
+	
+	return memo[i][j];
+}
+
+
+bool RobotInAGridDP(vvi & vv, int n, int m){
+	vvi memo(n, vi(m, -1));
+	bool ans = RobotInAGridDP(vv, memo, 0, 0, n-1, m-1);
+	return ans;
+}
+
+//TopDown returning the path
+//Complejidad temporal O(n * m) , Complejidad espacial O(n*m)
+int RobotInAGridDPWithPath(vvi & vv, vvi & memo, int i, int j, int n, int m){
 	if(i > n || j > m) return 0;
 	if(i == n && j == m){
 		return 1;
@@ -42,14 +79,34 @@ bool RobotInAGridDP(vvi & vv, vvi & memo, int i, int j, int n, int m){
 	return memo[i][j];
 }
 
-
-bool RobotInAGridDP(vvi & vv, int n, int m){
-	vvi memo(n, vi(m, -1));
-	return RobotInAGridDP(vv, memo, 0, 0, n-1, m-1);
+//asume que hay efectivamente un camino de (n-1, m-1) a (0,0)
+vector<pair<int, int> > getPath(vvi & memo, int n, int m){
+	vector<pair< int, int> > ans;
+	int i = n-1;
+	int j = m-1;
+	while(i >= 0 && j >= 0){
+		ans.push_back(make_pair(i,j));
+		if(memo[i][j-1] == 1){
+			j--;	
+		}else{
+			i--;
+		}
+	}
+	return ans;
 }
 
-//Bottom Up
-bool RobotInAGridBottomUp(vvi & vv, int n, int m){
+vector<pair<int, int> > RobotInAGridDPWithPath(vvi & vv, int n, int m){
+	vector<pair<int, int> > ans;
+	vvi memo(n, vi(m, -1));
+	int isPath = RobotInAGridDP(vv, memo, 0, 0, n-1, m-1);
+	print(memo, n, m);
+	if(isPath)
+		ans = getPath(memo, n, m);
+	return ans;
+}
+
+//Iterativo
+bool RobotInAGridIterative(vvi & vv, int n, int m){
 	vvi memo(n, vi(m, -1));
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < m; j++){
@@ -63,8 +120,8 @@ bool RobotInAGridBottomUp(vvi & vv, int n, int m){
 	return memo[n-1][m-1];
 }
 
-//Bottom Up usando solo un vector como memo
-bool RobotInAGridBottomUpFun(vvi & vv, int n, int m){
+//Iterativo usando solo un vector como memo
+bool RobotInAGridIterativeFun(vvi & vv, int n, int m){
 	vi memo(m, -1);//just for fun
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < m; j++){
@@ -78,14 +135,13 @@ bool RobotInAGridBottomUpFun(vvi & vv, int n, int m){
 	return memo[m-1];
 }
 
-void print(vvi & vv, int n, int m){
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < m; j++)
-			cout << vv[i][j] << " ";
-		cout << endl;
-	}
-	cout << endl;
+
+void printPath(vector<pair<int, int> > & path){
+	for(auto p : path)
+		cout << "(" << p.first << "," << p.second << ") ";
+	cout << endl; 
 }
+
 
 int main(){
 	
@@ -98,8 +154,11 @@ int main(){
 	
 	CYESNO(RobotInAGrid(vv, n, m))
 	CYESNO(RobotInAGridDP(vv, n, m))
-	CYESNO(RobotInAGridBottomUp(vv, n, m))
-	CYESNO(RobotInAGridBottomUpFun(vv, n, m))
+	CYESNO(RobotInAGridIterative(vv, n, m))
+	CYESNO(RobotInAGridIterativeFun(vv, n, m))
+	cout << endl;
+	vector<pair<int, int> > pair = RobotInAGridDPWithPath(vv, n, m);
+	printPath(pair);
 	
 	return 0;
 }
--- 8RecursionAndDynamicProgramming/3MaginIndex.cpp
+++ 8RecursionAndDynamicProgramming/3MaginIndex.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <unordered_set>
 #include <unordered_map>
+#include <cassert>
 
 #define CYESNO(x) cout << (x ? "YES" : "NO") << endl;
 #define CN(x) cout << #x << " = " << x << endl;
@@ -15,14 +16,10 @@ int findMagicIndexBF(vector<int> & v){
 	return -1;
 }
 
+//sin repetidos: v estrictamente creciente
 int findMagicIndex(vector<int> & v, int start, int end){
 	if(start > end) return -1;
 	int mid = (start + end) / 2;
-	CN(start)
-	CN(end)
-	cout << "-" << endl;
-	CN(mid)
-		
 	if(v[mid] == mid) return mid;
 	if(v[mid] < mid){
 		return findMagicIndex(v, mid+1, end);
@@ -31,9 +28,7 @@ int findMagicIndex(vector<int> & v, int start, int end){
 	}
 }
 
-
 int findMagicIndex(vector<int> & v){
-	CN(v.size())
 	return findMagicIndex(v, 0, v.size()-1);
 }
 
@@ -41,34 +36,48 @@ int findMagicIndex(vector<int> & v){
 int findMagicIndexWithReps(vector<int> & v, int start, int end){
 	if(start > end) return -1;
 	int mid = (start + end) / 2;
-	CN(start)
-	CN(end)
-	cout << "-" << endl;
-	CN(mid)
-		
-	if(v[mid] == mid) return mid;
-	if(v[mid] < mid){
-		return findMagicIndexWithReps(v, mid+1, end);
-	}else{
-		return findMagicIndexWithReps(v, start, mid-1);
-	}
+	int x = v[mid];
+	if(x == mid) return x;
+	
+	int leftEnd = min(mid-1, x);
+	int ansLeft = findMagicIndexWithReps(v, start, leftEnd);
+	if(ansLeft != -1) return ansLeft;
+	
+	int rightStart = max(mid+1, x);
+	int ansRight = findMagicIndexWithReps(v, rightStart, end);
+	if(ansRight != -1) return ansRight;
+	
+	return -1;
 }
 
 
 int findMagicIndexWithReps(vector<int> & v){
-	CN(v.size())
 	return findMagicIndexWithReps(v, 0, v.size()-1);
 }
 
 int main(){
 	
+	//sin elementos repetidos
 	vector<int> v = {-10, -3, 0, 3, 8, 9, 12, 15};
-	
 	cout << findMagicIndex(v) << endl;
+	v = {-2, -1, 2, 3, 5, 8, 9, 12, 13, 16};
+	cout << findMagicIndex(v) << endl;
+	//con repetidos
+	v = {0, 0, 4, 4, 4};
+	cout << findMagicIndexWithReps(v) << endl;
+	assert(findMagicIndexWithReps(v)==0);//podría también 4 (cambiando el algoritmo, claro) 
 	
-	vector<int> w = {-2, -1, 2, 3, 5, 8, 9, 12, 13, 16};
+	v = {0, 0, 4, 4, 5};
+	cout << findMagicIndexWithReps(v) << endl;
+	assert(findMagicIndexWithReps(v)==0);
 	
-	cout << findMagicIndex(w) << endl;
+	v = {-1, 0, 4, 4, 5};
+	cout << findMagicIndexWithReps(v) << endl;
+	assert(findMagicIndexWithReps(v)==-1);
 	
+	v = {-1, 0, 4, 4, 4};
+	cout << findMagicIndexWithReps(v) << endl;
+	assert(findMagicIndexWithReps(v)==4);
+
 	return 0;
 }
